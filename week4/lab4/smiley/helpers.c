23740245434d696841959b13b1177226d379c510
lcw001 <lcw001@carolina.rr.com>
2023-08-18 12:34:31 -0400

    adding week4 folder

---
+++ week4/lab4/smiley/helpers.c
@@ -0,0 +1,26 @@
+#include "helpers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Change all black pixels to a color of your choosing
+
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < height; column++)
+        {
+            if (image[column][row].rgbtBlue == 0 &&
+                image[column][row].rgbtGreen == 0 &&
+                image[column][row].rgbtRed == 0)
+            {
+                image[column][row].rgbtBlue = 255;
+                image[column][row].rgbtGreen = 0;
+                image[column][row].rgbtRed = 0;
+            }
+        }
+    }
+}
+
+
+
